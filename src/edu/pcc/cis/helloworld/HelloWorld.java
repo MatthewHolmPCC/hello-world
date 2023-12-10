938b6aff7d6cb05bf2b6fc1f89c1b7cb2ef1a738
Matthew Holm <matthew.holm@pcc.edu>
2023-12-10 13:50:07 -0800

    Added header comment with author/version info.

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,20 @@
 package edu.pcc.cis.helloworld;
 
-public class HelloWorld {
+/**
+ * Hello World class for GitHub sample project setup.
+ *
+ * @author Matthew Holm
+ * @version 2023.12.10
+ */
+
+public class HelloWorld
+{
+    /**
+     * Say hello to the world
+     * @param args String array, Arguments for program.
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
 }
